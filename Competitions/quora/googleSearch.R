b680eb5990e2ae4a550cefdbdbd2a1128134aea7
pdashk <phi.ktn@gmail.com>
2017-04-03 01:27:29 -0700

    Add files via upload

--- Competitions/quora/googleSearch.R
+++ Competitions/quora/googleSearch.R
@@ -16,6 +16,8 @@ session <- html_session(url)
 searchResult <- html_text(html_nodes(session, css = ".st"))
 searchResult <- gsub(patter = "[^[:alpha:] ]", replacement = "", searchResult)
 
+searchResult <- searchResult[searchResult != ""] #sometimes Google returns the first text under .st element as a blank (maybe due to suggestions). This remoes blanks ands results seem OK
+
 return(searchResult)
 rm(a, url, session, searchResult)
 
